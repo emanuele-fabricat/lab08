5b7c4418251b29870b87cf13a9e0645e910e81b0
Emanuele Fabricat - emanuele.fabricat@studio.unibo.it <emanuele.fabricat@studio.unibo.it>
2024-11-11 12:54:51 +0100

    start 82

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,37 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.impl.DeathNoteImplementation;
+
+class TestDeathNote {
+    private DeathNoteImplementation diairy;
+
+    @BeforeEach
+    void setUp(){
+        diairy = new DeathNoteImplementation();
+    }
+
+    @Test
+    void testNumberRule(){
+        for(int i = 1; i > -4; i--){
+            try {
+                diairy.getRule(i);
+            } catch (IllegalArgumentException e) {
+                assert(e.getMessage() != null);
+                assert(e.getMessage() != "");
+                assert(e.getMessage() != " ");
+            }
+        }
+    }
+
+    @Test
+    void testEmptyRule(){
+        for (String rule : RULES) {
+            
+        }
+    }
 }
