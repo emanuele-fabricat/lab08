43eca00fed86f2ef73ac0198afb91dd897915f0a
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2024-11-14 17:54:40 +0100

    impleted all test of 82

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,37 +1,106 @@
 package it.unibo.deathnote;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.deathnote.impl.DeathNoteImplementation;
 
 class TestDeathNote {
-    private DeathNoteImplementation diairy;
+    private DeathNoteImplementation diary;
+    private String name;
+    private String anotherName;
+    private static final String DEFOULT_CAUSE = "heart attack";
+    private static final String CAUSE = "karting accident";
+    private static final String DEFOULT_DETAILS = "";
+    private static final String DETAILS = "ran for too long";
+    private static final int TIME_DEFOULT = 100;
+    private static final long LONG_TIME_DEFOULT = TIME_DEFOULT + 6000;
 
     @BeforeEach
     void setUp(){
-        diairy = new DeathNoteImplementation();
+        diary = new DeathNoteImplementation();
+        name = "Giacomo";
+        anotherName = "Carlo";
     }
 
     @Test
     void testNumberRule(){
         for(int i = 1; i > -4; i--){
             try {
-                diairy.getRule(i);
+                diary.getRule(i);
             } catch (IllegalArgumentException e) {
-                assert(e.getMessage() != null);
-                assert(e.getMessage() != "");
-                assert(e.getMessage() != " ");
+                assertNotNull(e.getMessage());
+                assertFalse(e.getMessage().isBlank());
             }
         }
     }
 
     @Test
     void testEmptyRule(){
-        for (String rule : RULES) {
-            
+        for (String rule : diary.RULES) {
+           assertNotNull(rule);
+           assertFalse(rule.isBlank());
         }
+    }
+
+    @Test
+    void testName(){
+        assertFalse(diary.isNameWritten(name));
+        diary.writeName(name);
+        assertTrue(diary.isNameWritten(name));
+        assertFalse(diary.isNameWritten(anotherName));
+        assertFalse(diary.isNameWritten(""));
+    }
+
+    @Test
+    void testCause(){
+        try {
+            diary.writeDeathCause("");
+            Assertions.fail("Wrote the cause before write the name");
+        } catch (IllegalArgumentException e) {
+            assertFalse(e.getMessage().isBlank());
+            assertNotNull(e.getMessage());
+        }
+        diary.writeName(anotherName);
+        assertEquals(DEFOULT_CAUSE, diary.getDeathCause(anotherName));
+        diary.writeName(name);
+        assertTrue(diary.writeDeathCause(CAUSE));
+        assertEquals(CAUSE, diary.getDeathCause(name));
+        try {
+            Thread.sleep(TIME_DEFOULT);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        diary.writeDeathCause(DEFOULT_CAUSE);
+        assertEquals(CAUSE, diary.getDeathCause(name));
+    }
+
+    @Test
+    void  testDetails(){
+        try {
+            diary.writeDetails("");
+            Assertions.fail("Wrote the details before write the name");
+        } catch (IllegalStateException e) {
+            assertFalse(e.getMessage().isBlank());
+            assertNotNull(e.getMessage());
+        }
+        diary.writeName(anotherName);
+        assert(diary.getDeathDetails(anotherName).isEmpty());
+        assert(diary.writeDetails(DETAILS));
+        assertEquals(DETAILS, diary.getDeathDetails(anotherName));
+        diary.writeName(name);
+        try {
+            Thread.sleep(LONG_TIME_DEFOULT);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        diary.writeDetails(DETAILS);
+        assertEquals(DEFOULT_DETAILS, diary.getDeathDetails(name));
     }
 }
