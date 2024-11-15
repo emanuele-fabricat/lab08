cbbb9d50e6cba5ee40e5a3bc182c24105a421fb2
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2024-11-15 19:05:24 +0100

    start to work on point 2 of 83

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -4,6 +4,7 @@ import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 /**
@@ -13,7 +14,7 @@ import java.util.Objects;
 public final class DrawNumberControllerImpl implements DrawNumberController {
 
     private final DrawNumber model;
-    private DrawNumberView view;
+    private ArrayList<DrawNumberView> view = new ArrayList<DrawNumberView>();
 
     /**
      * Builds a new game controller provided a game model.
@@ -27,17 +28,20 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
     @Override
     public void addView(final DrawNumberView view) {
         Objects.requireNonNull(view, "Cannot set a null view");
-        if (this.view != null) {
-            throw new IllegalStateException("The view is already set! Multiple views are not supported");
+        this.view.add(view);
+        for (DrawNumberView drawNumberView : this.view) {
+            drawNumberView.setController(this);
+            drawNumberView.start();
         }
-        this.view = view;
-        view.setController(this);
-        view.start();
     }
 
     @Override
     public void newAttempt(final int n) {
-        Objects.requireNonNull(view, "There is no view attached!").result(model.attempt(n));
+        if(!view.isEmpty()){
+            for(DrawNumberView drawNumberView : this.view){
+                 drawNumberView.result(model.attempt(n));
+            }
+        }
     }
 
     @Override
--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStdoutView.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStdoutView.java
@@ -1,5 +1,27 @@
 package it.unibo.mvc.view;
 
-public class DrawNumberStdoutView {
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberStdoutView implements DrawNumberView{
+
+    @Override
+    public void setController(DrawNumberController observer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setController'");
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'result'");
+    }
 
 }
